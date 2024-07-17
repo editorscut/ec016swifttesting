ccb938bf86f20e400a471918bf8dfe6588b84c12
Daniel H Steinberg <inquiries@dimsumthinking.com>
2024-07-17 16:57:35 -0400

    Initial code commit

---
+++ Chapter01/02/Sample/Sample/SampleApp.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+public let isConnected = false
+
+@main
+struct SampleApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
---
+++ Chapter01/02/Sample/SampleTests/SampleTests.swift
@@ -0,0 +1,9 @@
+import Testing
+@testable import Sample
+
+struct SampleTests {
+  
+  @Test func example() async throws {
+    #expect(isConnected)
+  }
+}
