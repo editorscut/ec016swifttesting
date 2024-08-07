d5b59c6076d3d6189a1ee88fc4bb6bfbc6421719
Daniel H Steinberg <inquiries@dimsumthinking.com>
2024-08-07 13:06:09 -0700

    Async section

--- Chapter04/04/RPNCalculator/RPNCalculatorTests/AsyncTests.swift
+++ Chapter04/04/RPNCalculator/RPNCalculatorTests/AsyncTests.swift
@@ -1,19 +1,36 @@
 import Testing
 @testable import RPNCalculator
 
-@Suite("Async tests for fake code")
+@Suite("Async tests",
+       .disabled())
 struct AsyncTests {
   @Test
-  func twoTimesANumber()  throws {
-    Task {
-      let result = await timesTwo(2.3)
-      #expect(result == 4)
-    }
+  func twoTimesANumber() async {
+    let result = await timesTwo(2.3)
+    #expect(result == 4.6)
   }
   
   @Test
-  func twoTimesANumberB() async throws {
-    #expect(await timesTwo(2.3) == 4.6)
+  func twoTimesAnotherNumber() async {
+    #expect(await timesTwo(7.9) == 15.8)
+  }
+  
+  @Test(.timeLimit(.minutes(1)),
+        .bug("http://example.com",
+             "longTimesTwo always takes more than 60 seconds"),
+        .disabled("Performance issue with longOperation()"))
+  func longOperation() async {
+    #expect(await longTimesTwo(4.7) == 9.4)
+  }
+  
+  @MainActor
+  @Test
+  func closureAsync() async {
+    let result = await withCheckedContinuation {continuation in
+      closureTimesTwo(5.8, completion: {(d: Double) -> Void in
+        continuation.resume(returning: d)})
+    }
+    #expect(result == 11.6)
   }
 }
 
